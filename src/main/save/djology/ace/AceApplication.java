d3d7de39db9f1c096fda08a31d527385ec2a5115
djology <djology.w@gmail.com>
2021-10-09 16:21:01 +0800

    Initial commit

---
+++ src/main/save/djology/ace/AceApplication.java
@@ -0,0 +1,19 @@
+package djology.ace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @DESC: 记录此时天时地利，并备份至Github
+ * @Author: djology.w
+ * @Date: 2021/10/9 16:13
+ */
+
+@SpringBootApplication
+public class AceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AceApplication.class, args);
+    }
+
+}
